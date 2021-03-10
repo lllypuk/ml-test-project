fbb2876fc6729731328ab818bcfd724d16e3350f
ashatrov <NikonD3100>
2021-03-10 17:54:10 +0500

    3 commit

--- src/main.py
+++ src/main.py
@@ -1,18 +1,17 @@
 import pandas as pd
 
 
-def read_and_print_file(uri):
-    data_frame = pd.read_csv(uri)
-    print(data_frame)
-    return data_frame
-
-
 if __name__ == '__main__':
-    df = read_and_print_file('titanic.csv')
-    print(df.dtypes)
-    print(df.describe().iloc[:, :2])
-    print(df.shape)
-    print(df.isnull().sum())
-    print(df.isnull().mean())
-    print(df.isnull().sum(axis=1).loc[:10])
-
+    df = pd.read_csv('titanic.csv')
+    # print(df)
+    # print(df.dtypes)
+    # print(df.describe().iloc[:, :2])
+    # print(df.shape)
+    # print(df.isnull().sum())
+    # print(df.isnull().mean())
+    # print(df.isnull().sum(axis=1).loc[:10])
+    # mask = df.isnull().any(axis=1)
+    # print(mask.head())
+    # print(df[mask].body.head())
+    print(df.sex.value_counts(dropna=False))
+    print(df.embarked.value_counts(dropna=True))
