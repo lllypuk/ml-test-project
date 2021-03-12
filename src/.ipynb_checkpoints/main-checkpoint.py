ed73a67db14d2b1427334d833427fe142ebd97a2
ashatrov <NikonD3100>
2021-03-12 17:59:44 +0500

    5 commit

---
+++ src/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('titanic.csv')
+    # print(df)
+    # print(df.dtypes)
+    # print(df.describe().iloc[:, :2])
+    # print(df.shape)
+    # print(df.isnull().sum())
+    # print(df.isnull().mean())
+    # print(df.isnull().sum(axis=1).loc[:10])
+    # mask = df.isnull().any(axis=1)
+    # print(mask.head())
+    # print(df[mask].body.head())
+    # print(df.sex.value_counts(dropna=False))
+    # print(df.embarked.value_counts(dropna=False))
+    # print(df.info())
+    # print(df.describe())
+    # print(df['body'].head())
+    # print(df.loc[0:5, ['body']])
+    # print(df.iloc[:,2:4])
+    print(df[df['survived'] == 1].describe())
